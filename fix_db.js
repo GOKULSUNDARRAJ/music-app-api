36d1428c859cab0010cfc1658b99c59d90ef9933
GOKULSUNDARRAJ <gokulsundar4545@gmail.com>
2026-07-20 16:11:58 +0530

    Update fix_db.js to use config

--- fix_db.js
+++ fix_db.js
@@ -1,11 +1,5 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  logging: false
-});
+const sequelize = require('./config/database');
 
 async function run() {
   try {
